20f79b899aa512f258435242629006686f2b0173
Adam-Barto <adam.jbarto@gmail.com>
2023-05-29 15:00:04 -0400

    Basic Stats Outline

---
+++ characterBuilder.py
@@ -0,0 +1,16 @@
+MAX_POINTS = 30
+Stats = {
+    "Body": [0,
+             {
+                 "Strength": [0, {"Tone": 0, "Endurance": 0}],
+                 "Dexterity": [0, {"Agility": 0, "Speed": 0}],
+                 "Constitution": [0, {"Resolve": 0, "Bulk": 0}]
+             }],
+    "Mind": [0,
+             {
+                 "Intelligence": [0, {"Recall": 0, "Deduction": 0}],
+                 "Wisdom": [0, {"Understanding": 0, "Faith": 0}],
+                 "Charisma": [0, {"Urge": 0, "Instinct": 0}]
+             }]
+}
+
