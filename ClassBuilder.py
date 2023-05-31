1b6ec897a4ae9d0bceaacf6821ebc68d416a58ad
Adam-Barto <adam.jbarto@gmail.com>
2023-05-31 14:07:06 -0400

    Moved the Interface to the Class File

--- ClassBuilder.py
+++ ClassBuilder.py
@@ -1,3 +1,5 @@
+import tkinter as tk
+
 MAX_POINTS = 30
 Stats = {
     "Body": [0,
@@ -36,4 +38,23 @@ class Character:
         return spent == self.MAX_POINTS
 
 
+class Interface:
+    def __init__(self):  # menu, body_display, body_and_mind_scale, mind_display
+        self.menu = tk.Tk()
+        self.body_display = tk.Label(self.menu, text=0)
+        self.mind_display = tk.Label(self.menu, text=0)
+        self.body_and_mind_scale = tk.Scale(self.menu, from_=6, to=24, orient=tk.HORIZONTAL, showvalue=False)
+        self.name = tk.Entry(self.menu)
+        self.army = tk.Entry(self.menu)
 
+        self.menu.title('Quest')
+        # Making a Window
+        tk.Label(self.menu, text='Name: ').grid(row=0)
+        tk.Label(self.menu, text='Army Name: ').grid(row=1)
+        tk.Label(self.menu, text='Body').grid(row=2)
+        self.body_display.grid(row=3)
+        self.body_and_mind_scale.grid(row=2, column=1)
+        tk.Label(self.menu, text='Mind').grid(row=2, column=2)
+        self.mind_display.grid(row=3, column=2)
+        self.name.grid(row=0, column=1)
+        self.army.grid(row=1, column=1)
