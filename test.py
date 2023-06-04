57474ce6ee0931a3a56eaf62c8fde4afc10b2cd7
Adam-Barto <adam.jbarto@gmail.com>
2023-06-04 13:27:58 -0400

    Test for Build Control List added

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import functions as f
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_build_control_list(self):
+        expected = {'Agility': 1, 'Bulk': 1,
+                    'Deduction': 1, 'Endurance': 1,
+                    'Faith': 1, 'Instinct': 1,
+                    'Recall': 1, 'Resolve': 1,
+                    'Speed': 1, 'Tone': 1,
+                    'Understanding': 1, 'Urge': 1}
+        actual = f.build_control_list()
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
