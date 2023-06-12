e739c36519b03569fbe6feaed33dbd3ab9c3d6bb
Adam-Barto <adam.jbarto@gmail.com>
2023-06-12 11:41:19 -0400

    Added Dict for army value Calculations

--- functions.py
+++ functions.py
@@ -1,19 +1,49 @@
 MAX_POINTS = 30
 Stats = {
-    "Body": [0,
+    "Body": [6,
              {
-                 "Strength": [0, {"Tone": 0, "Endurance": 0}],
-                 "Dexterity": [0, {"Agility": 0, "Speed": 0}],
-                 "Constitution": [0, {"Resolve": 0, "Bulk": 0}]
+                 "Strength": [2, {"Tone": 1, "Endurance": 1}],
+                 "Dexterity": [2, {"Agility": 1, "Speed": 1}],
+                 "Constitution": [2, {"Resolve": 1, "Bulk": 1}]
              }],
-    "Mind": [0,
+    "Mind": [6,
              {
-                 "Intelligence": [0, {"Recall": 0, "Deduction": 0}],
-                 "Wisdom": [0, {"Understanding": 0, "Faith": 0}],
-                 "Charisma": [0, {"Urge": 0, "Instinct": 0}]
+                 "Intelligence": [2, {"Recall": 1, "Deduction": 1}],
+                 "Wisdom": [2, {"Understanding": 1, "Faith": 1}],
+                 "Charisma": [2, {"Urge": 1, "Instinct": 1}]
              }]
 }
 # 1              2              3
+Base_Stats = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
+Sub_Stats = {
+    "Tone": 1, "Endurance": 1,
+    "Agility": 1, "Speed": 1,
+    "Resolve": 1, "Bulk": 1,
+
+    "Recall": 1, "Deduction": 1,
+    "Understanding": 1, "Faith": 1,
+    "Urge": 1, "Instinct": 1
+}
+
+Army_Numbers = {
+    "Infantry": 0,
+    "Beast": 0,
+    "Heavy": 0,
+    "Healer": 0,
+    "Casters": 0,
+    "Artillery": 0,
+    "Fortifications": 0
+}
+
+Army_Calculation = {
+    "Infantry": ["Endurance", "Speed", "Resolve"],
+    "Beast": ["Understanding", "Instinct", "Deduction"],
+    "Heavy": ["Bulk", "Tone", "Urge"],
+    "Healer": ["Faith", "Endurance", "Agility"],
+    "Casters": ["Recall", "Resolve", "Urge"],
+    "Artillery": ["Bulk", "Deduction", "Endurance"],
+    "Fortifications": ["Resolve", "Recall", "Tone"]
+}
 
 
 def build_control_list(data=None):  # This makes us have an easy to manipulate list.
@@ -26,3 +56,8 @@ def build_control_list(data=None):  # This makes us have an easy to manipulate l
             for (key, value) in zip(stat2[1].keys(), stat2[1].values()):
                 dev_holder.update({key: value})
     return dev_holder
+
+
+
+
+
